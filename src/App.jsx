571aab4e32ab6b0a0d477f682df4ca69eddecf67
Dev-Aniweb <anilkumarve51@gmail.com>
2025-09-22 15:13:33 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react"
+import Navbar from "./Components/Navbar"
+import Newsboard from "./Components/Newsboard"
+
+
+
+const App = () => {
+
+  return (
+    <div>
+<Navbar />
+<Newsboard />
+
+    </div>
+  )
+}
+
+export default App
